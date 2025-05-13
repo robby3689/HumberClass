b7f17f865b610a06d679999698318dfb1b1824c3
robby3689 <sachin.jmu7@gmail.com>
2025-05-13 10:14:50 -0400

    Class Work

--- script.js
+++ script.js
@@ -1,71 +1,74 @@
-// // // alert("Hello Singh");
+// // // // alert("Hello Singh");
 
-// // var x = 10;
-// // var x = x +2;
-// // // alert(x);
+// // // var x = 10;
+// // // var x = x +2;
+// // // // alert(x);
 
-// // var hello = "HELLOOWWWWW ";
-// // var world = "WORLDDDDD"
+// // // var hello = "HELLOOWWWWW ";
+// // // var world = "WORLDDDDD"
 
-// // hello += world;
+// // // hello += world;
 
-// // alert(hello);
+// // // alert(hello);
 
-// // console.log();
+// // // console.log();
+// // // var isRaining = true;
+// // // if(isRaining == true)
+// // // {
+// // // 	alert("Ground Is Wet");
+// // // }
+// // // if(isRaining == false){
+// // // 	alert("DRY GROUND");
+// // // }
+
+// // // var price;
+
+// // // console.log(price);
+// // // if(price > 5){
+// // // 	alert("Greater than 5")
+// // // }
+// // // if(price < 5){
+// // // 	alert("Smaller than 5")
+// // // }
+
+// // // // =  assign value
+// // // // == comparing values
+// // // // === compares datatypes
+
+
 // // var isRaining = true;
-// // if(isRaining == true)
-// // {
-// // 	alert("Ground Is Wet");
+
+// // if(isRaining === true){
+// // 	alert("blah blah")
 // // }
-// // if(isRaining == false){
-// // 	alert("DRY GROUND");
+// // else if(isRaining === false){
+// // 	alert("false")
 // // }
-
-// // var price;
-
-// // console.log(price);
-// // if(price > 5){
-// // 	alert("Greater than 5")
-// // }
-// // if(price < 5){
-// // 	alert("Smaller than 5")
-// // }
-
-// // // =  assign value
-// // // == comparing values
-// // // === compares datatypes
+// // if
 
 
-// var isRaining = true;
+// var fruit = "banana";
 
-// if(isRaining === true){
-// 	alert("blah blah")
+// var userFruit = prompt("Please enter a fruit", "Grape");
+
+// // Check the user input
+// switch (userFruit) {
+//   case "apple":
+//     alert("This is apple");
+//     break;
+//   case "banana":
+//     alert("This is a banana");
+//     break;
+//   case "pineapple":
+//     alert("This is a pineapple");
+//     break;
+//   case "cherry":
+//     alert("This is a cherry");
+//     break;
+//   default:
+//     alert("Unknown fruit: " + userFruit);
+//     break;
 // }
-// else if(isRaining === false){
-// 	alert("false")
-// }
-// if
 
-
-var fruit = "banana";
-
-var userFruit = prompt("Please enter a fruit", "Grape");
-
-// Check the user input
-switch (userFruit) {
-  case "apple":
-    alert("This is apple");
-    break;
-  case "banana":
-    alert("This is a banana");
-    break;
-  case "pineapple":
-    alert("This is a pineapple");
-    break;
-  case "cherry":
-    alert("This is a cherry");
-    break;
-  default:
-    alert("Unknown fruit: " + userFruit);
-    break;
-}
+// var isConfirmed = confirm("Is this true?");
+// alert(isConfirmed);
